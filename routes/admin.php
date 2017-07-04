f3821a877f3ba50c76616fcb011bcb1e365b5f41
thienth32 <thienth32@gmail.com>
2017-07-04 20:56:51 +0700

    update finding maximum number

--- routes/admin.php
+++ routes/admin.php
@@ -1,34 +1,31 @@
 <?php 
 
-Route::get('/', function(){
+// Route::get('/', function(){
 
-	dd('welcome to admin page');
-});
-Route::get('/add-category', function(){
+// 	dd('welcome to admin page');
+// });
+// Route::get('/add-category', function(){
 
-	dd('admin => add category');
-});
+// 	dd('admin => add category');
+// });
 
-Route::group(['prefix' => 'category'], function(){
-	Route::get('create', function(){
-		dd('create category');
-	});
+// Route::group(['prefix' => 'category'], function(){
+// 	Route::get('create', function(){
+// 		dd('create category');
+// 	});
 
-	Route::get('save', function(){
-		dd('save category');
-	});
-});
-Route::get('login', function(){
-	dd('login page');
-})->name('login');
-Route::group(['prefix' => 'post', 'middleware' => 'auth'], function(){
-	Route::get('create', function(){
-		dd('create post');
-	});
+// 	Route::get('save', function(){
+// 		dd('save category');
+// 	});
+// });
+// Route::group(['prefix' => 'post', 'middleware' => 'auth'], function(){
+// 	Route::get('create', function(){
+// 		dd('create post');
+// 	});
 
-	Route::get('save', function(){
-		dd('save post');
-	});
-});
+// 	Route::get('save', function(){
+// 		dd('save post');
+// 	});
+// });
 
  ?>
--- routes/web.php
+++ routes/web.php
@@ -11,24 +11,34 @@
 |
 */
 
-Route::get('/', function () {
-	dd(route('route-parameter', ['name' => 't3h', 'a' => '123456']));
-    return view('welcome');
-})->name('homepage');
-Route::get('/route-parameter-thienth/{a}/{name}', function($a1){
+// Route::get('/', function () {
+// 	dd(route('route-parameter', ['name' => 't3h', 'a' => '123456']));
+//     return view('welcome');
+// })->name('homepage');
+// Route::get('/route-parameter-thienth/{a}/{name}', function($a1){
 
-	// process a->z
-	dd(route('homepage'));
-	return redirect(route('homepage'));
-})->name('route-parameter');
-Route::get('/optional-parameter/{a?}/{b}', function($a1 = "thienth"){
+// 	// process a->z
+// 	dd(route('homepage'));
+// 	return redirect(route('homepage'));
+// })->name('route-parameter');
+// Route::get('/optional-parameter/{a?}/{b}', function($a1 = "thienth"){
 
-	dd($a1);
-});
+// 	dd($a1);
+// });
 
-Route::get('user/{name}', function ($name) {
-    dd($name);
-})->where('name', '[A-Za-z]+');
+// Route::get('user/{name}', function ($name) {
+//     dd($name);
+// })->where('name', '[A-Za-z]+');
+Route::get('maximum/{a?}/{b?}/{c?}', function($a = 0, $b = 0, $c = 0){
 
+	$max = $a >= $b ? $a : $b;
+	$max = $max >= $c ? $max : $c;
+	dd('So lon nhat la: ' . $max);
+	
+})->where([
+		'a' => '[0-9]+',
+		'b' => '[0-9]+',
+		'c' => '[0-9]+',
+	]);
 
 
