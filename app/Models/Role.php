b368db0ec2b1df1886577fed673918619f68fffc
thienth32 <thienth32@gmail.com>
2017-07-11 20:15:52 +0700

    update

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -7,4 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Role extends Model
 {
  	protected $table = 'roles';
+ 	public function users(){
+        return $this->belongsToMany('App\User', 'user_role_xref');
+    }
 }
