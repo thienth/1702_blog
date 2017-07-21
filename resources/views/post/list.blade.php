f2bb76e383e0f6ae763442649420df41fdea7837
thienth32 <thienth32@gmail.com>
2017-07-21 18:17:36 +0700

    update

--- resources/views/post/list.blade.php
+++
@@ -1,9 +0,0 @@
-<link rel="stylesheet" type="text/css" href="{{asset('css/main.css')}}">
-
-{{$age}}
-<br>
-
-<a href="{{route('homepage')}}" title="">Homepage</a>
-@foreach ($listPost as $element)
-	<div>{{$element->title}}</div>
-@endforeach
