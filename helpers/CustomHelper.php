a9f557ff46aaaed67dc13268c34beeffaea2979a
thienth32 <thienth32@gmail.com>
2017-07-26 20:40:01 +0700

    update recursive function

--- helpers/CustomHelper.php
+++ helpers/CustomHelper.php
@@ -1,9 +1,23 @@
 <?php 
-if(!function_exists('recursive_category')){
-	function recursive_category($listCate){
-		echo "t3h - 1702";
-	}
+if(!function_exists('get_options')){
+
+  function get_options($array, $parent=0, $indent="", $forget = null) {
+      
+      // Return variable
+      $return = array();
+      for ($i=0; $i < count($array); $i++) {
+          $val = $array[$i];
+
+        	if($val->parent_id == $parent && $val->id != $forget) {
+          	$return["x".$val->id] = $indent.$val->name;
+          	$return = array_merge($return, get_options($array, $val->id, $indent."--", $forget));
+          }
+      }
+
+      return $return;
+  }
 }
+
 
 
 
