b7a35bf4c9556c98fda2efe0498081d0575f9297
thienth32 <thienth32@gmail.com>
2017-07-28 20:48:33 +0700

    update

--- resources/views/admin/cate/form.blade.php
+++ resources/views/admin/cate/form.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.admin')
 @section('title', 'Category management')
 @section('content')
+	hello thienth
 	<div class="col-sm-6 col-sm-offset-3">
 		<form action="{{route('cate.save')}}" method="post" novalidate>
 			{{csrf_field()}}
